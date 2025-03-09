b613651cd14318c648eb71b1708c1c16122e1b28
Moehtet-hlaing <seoyuzuki#2310Mon>
2025-03-09 23:55:48 +0630

    bg img added

--- src/components/hero/GridImage.jsx
+++ src/components/hero/GridImage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import bgImg from "../../assets/grid.png";
 
 const GridImage = () => {
+  const backgroundGrid = {
+    backgroundImage: `url(${bgImg})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed", // Equivalent to bg-fixed
+    position: "relative", // Equivalent to relative
+    zIndex: 20, // Equivalent to z-20
+    height: "640px", // Default height h-[640px]
+    maxWidth: "1920px", // max-w-[1920px]
+    margin: "0 auto", // mx-auto
+    width: "100%",
+  };
   return (
-    <div className=" xl:bg-[url('./assets/grid.png')] xl:bg-center xl:bg-repeat-y max-w-[1280px] mx-auto bg-[url('./assets/grid.png')] bg-center bg-repeat-y h-[640px] fixed top-0 left-0 right-0 bottom-0 z-10">
+    <div style={backgroundGrid} className="">
     </div>
   )
 }
